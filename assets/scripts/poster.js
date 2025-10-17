4bff9bec211f85327977ac5828200fbae80665c4
Benjamin Sykes <33205078+sykeben@users.noreply.github.com>
2025-10-17 14:12:58 -0400

    Upgrade poster generator.

--- assets/scripts/poster.js
+++ assets/scripts/poster.js
@@ -3,7 +3,9 @@ document.addEventListener("DOMContentLoaded", () => {
 
     // Get control inputs.
     const inputTitle = document.getElementById("input-title");
-    const inputSubtitle = document.getElementById("input-subtitle");
+    const inputSubtitle1 = document.getElementById("input-subtitle1");
+    const inputSubtitle2 = document.getElementById("input-subtitle2");
+    const inputSubtitle3 = document.getElementById("input-subtitle3");
     const inputQRPage = document.getElementById("input-qrpage");
 
     // Get control buttons.
@@ -12,7 +14,9 @@ document.addEventListener("DOMContentLoaded", () => {
 
     // Get page elements.
     const pageTitle = document.getElementById("page-title");
-    const pageSubtitle = document.getElementById("page-subtitle");
+    const pageSubtitle1 = document.getElementById("page-subtitle1");
+    const pageSubtitle2 = document.getElementById("page-subtitle2");
+    const pageSubtitle3 = document.getElementById("page-subtitle3");
     const pageQRCode = document.getElementById("page-qrcode");
     const pageQRURL = document.getElementById("page-qrurl");
     const pageQRPage = document.getElementById("page-qrpage");
@@ -25,8 +29,10 @@ document.addEventListener("DOMContentLoaded", () => {
     generateBtn.addEventListener("click", () => {
 
         // Update text.
-        pageTitle.textContent = inputTitle.value || "???";
-        pageSubtitle.textContent = inputSubtitle.value || "?";
+        pageTitle.textContent = inputTitle.value || "";
+        pageSubtitle1.textContent = inputSubtitle1.value || "";
+        pageSubtitle2.textContent = inputSubtitle2.value || "";
+        pageSubtitle3.textContent = inputSubtitle3.value || "";
         pageQRPage.textContent = inputQRPage.value || "bsod";
 
         // Update QR code.
