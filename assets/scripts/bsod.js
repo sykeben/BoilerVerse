06e525fbeb1e04dbcd6d932766dcf48d888542b8
Benjamin Sykes <33205078+sykeben@users.noreply.github.com>
2025-10-17 14:05:21 -0400

    Deploy patch notes.

--- assets/scripts/bsod.js
+++ assets/scripts/bsod.js
@@ -1,10 +1,33 @@
 // He's dead jim!
 
+// Random integer getter (min and max included).
+function getRandom(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+// Hex formatter.
+function formatHex(value, digits) {
+    return value.toString(16).toUpperCase().padStart(digits, '0');
+}
+
 // Randomizes the error code.
+const codeDesired = getRandom(0xA, 0xF);
+let codeCount = -1;
 function randomizeError() {
-    const codeValue = Math.floor(Math.random() * 0xFFFFFF);
-    const codeText = codeValue.toString(16).toUpperCase().padStart(6, '0');
-    document.getElementById('error-code').innerText = codeText;
+
+    // Update values.
+    codeCount = (codeCount + 1) % (0xF + 1);
+    const codeValue = getRandom(0x0000, 0xFFFF);
+    const codeTarget = formatHex(codeDesired, 1);
+    const codePrefix = formatHex(codeCount, 1);
+    const codeSuffix = formatHex(codeValue, 5);
+    document.getElementById('error-code').innerText = `${codeTarget}${codePrefix}${codeSuffix}`;
+
+    // Redirect to the patch notes if we're at or past the target.
+    if (codeCount >= codeDesired) {
+        window.location.href = 'patches.html';
+    }
+
 }
 
 // Insert error code into the page & bind events.
